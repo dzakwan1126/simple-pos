43ebba0aad4cfceb2785e6b676917934b9b3e8dc
dzakwan1126 <dzakwanmuhammad6@gmail.com>
2021-03-12 20:49:53 +0700

    first

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © <a href="https://www.instagram.com/amelia.ghp/" target="_blank">Amelia Galuh Hartono Putri</a></span>
+		</div>
+	</div>
+</footer>
